27bd7087b741515a32c1850a6f2d6bf29aae6b03
migmfreitas <39192495+migmfreitas@users.noreply.github.com>
2018-05-11 15:34:23 +0100

    Add files via upload

---
+++ X-Y_controllers.js
@@ -0,0 +1,10 @@
+//  Control X an Y independently:
+//  Create a Shape
+//  Add 2 Slider Controls. Name them X and Y.
+//  Alt-Click the Stopwatch and Link them to the respective properties of the shape.
+//  Add the following expression to the scale:
+
+x = effect("x")("Slider");
+y = effect("y")("Slider");
+
+[x, y]
